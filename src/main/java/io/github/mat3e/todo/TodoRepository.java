af66e37e68bf99c0640eac0dd8340d01c07d3ff9
Pawel Kita <kitapawel@gmail.com>
2020-05-12 17:04:00 +0200

    Servlet and Repository functionality added in the Todo package.

---
+++ src/main/java/io/github/mat3e/todo/TodoRepository.java
@@ -0,0 +1,18 @@
+package io.github.mat3e.todo;
+
+import io.github.mat3e.HibernateUtil;
+
+import java.util.List;
+
+public class TodoRepository {
+    List<Todo> findAll(){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Todo", Todo.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+}
