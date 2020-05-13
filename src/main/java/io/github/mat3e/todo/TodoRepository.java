3bebfd7f437ed8bee82598e36015283227069d8e
Pawel Kita <kitapawel@gmail.com>
2020-05-13 21:47:39 +0200

    Finished TODO functionality.

--- src/main/java/io/github/mat3e/todo/TodoRepository.java
+++ src/main/java/io/github/mat3e/todo/TodoRepository.java
@@ -14,5 +14,29 @@ public class TodoRepository {
         transaction.commit();
         session.close();
         return result;
+        }
+
+    Todo toggleTodo(Integer id){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.get(Todo.class, id);
+        result.setDone(!result.isDone());
+
+        transaction.commit();
+        session.close();
+        return result;
     }
+
+    Todo addTodo (Todo newTodo){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        session.persist(newTodo);
+
+        transaction.commit();
+        session.close();
+        return newTodo;
+    }
+
 }
